b7b32d93c81581d9ac7ff311222eb7919c848f3c
RadHamm <stevenphamm@hotmail.com>
2024-11-30 00:07:30 -0500

    Part 4 clone and add new property

--- script.js
+++ script.js
@@ -11,25 +11,29 @@ let student = {
 };
 
 console.log(student.name, student.age);
-
+// call method for basic info
 console.log(student.displayInfo());
 
-
+// Part 2 convert student object into string
 let studentJSON = JSON.stringify(student);
 console.log(studentJSON);
-
+// convert json string back into JS object
 let newStudent = JSON.parse(studentJSON);
 console.log(newStudent);
-
+// compares new to original
 console.log(newStudent === studentJSON);
 
+//Part 3 1.1 extract name and course from student object
 let { name, courses } = student;
 console.log(name);
 console.log(courses);
-
+// 1.2 create array and extract first two scores
 let scores = [90, 80, 70, 60, 95, 85, 75]
 let [score1, score2] = scores;
 console.log(score1);
 console.log(score2);
 
+// Part 4 clone object student and add a new property
+let clonedStudent = { ...student, graduationYear: 2026 };
+console.log(clonedStudent);
 

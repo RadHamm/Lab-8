b45912d710ebe5547387ec81073f4dcb8cecd487
RadHamm <stevenphamm@hotmail.com>
2024-11-29 23:30:32 -0500

    Part 3 1.1 1.2 complete

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// Part 1.1 Create student object
 let student = {
     name: "Steven Hamm",
     age: 30,
     enrolled: true,
     courses: ["Javascript", "Web Design", "Writing", "Operating Systems"],
+    // Method to display basic info
     displayInfo: function() {
         return `Name: ${this.name}, Age: ${this.age}`;
     }
@@ -20,3 +22,14 @@ let newStudent = JSON.parse(studentJSON);
 console.log(newStudent);
 
 console.log(newStudent === studentJSON);
+
+let { name, courses } = student;
+console.log(name);
+console.log(courses);
+
+let scores = [90, 80, 70, 60, 95, 85, 75]
+let [score1, score2] = scores;
+console.log(score1);
+console.log(score2);
+
+
